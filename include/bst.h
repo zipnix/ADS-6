f5a41b21847b1ca2c7613346ae7768386734a5e4
zipnix <78799489+zipnix@users.noreply.github.com>
2021-05-25 16:56:28 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -8,57 +8,58 @@ class BST {
   struct Node {
     T value;
     int count;
-    Node * left;
-    Node * right;
+    Node* left;
+    Node* right;
   };
-  
+ 
  private:
-  Node* root;
-  Node* addNode (Node *root, T value) {
-    if (root == nullptr) {
-      root = new Node;
-      root->value = value;
-      root->count = 1;
-      root->left = root−>right = nullptr;
-    } else if (root−>value > value) {
-      root->left = addNode(root−>left, value);
-    } else if (root−>value < value) {
-      root->right = addNode(root−>right, value);
-    } else
-      root->count++;
-    return root;
+ Node* root;
+ Node* addNode(Node *root, T value) {
+  if (root == nullptr) {
+   root = new Node;
+   root->value = value;
+   root->count = 1;
+   root->left = root−>right = nullptr;
+  } else if (root−>value > value) {
+   root->left = addNode(root−>left, value);
+  } else if (root−>value < value) {
+   root->right = addNode(root−>right, value);
+  } else {
+   root->count++;
   }
-  int searchNode(Node *root, T value) {
-    if (root == nullptr)
-      return 0;
-    else if (root->value > value)
-      return searchNode(root->left, value);
-    else if (root->value < value)
-      return search(root->right, value);
-    else
-      return root->count;
-  }
-  int getheight(Node *root) {
-    if (root == nullptr)
-      return 0;
-    if (root->left == nullptr && root->right == nullptr)
-      return 0;
-    int lh = depthTree(root->left);
-    int rh = depthTree(root->right);
-    return (lh > rh ? lh : rh) + 1;
-  }
+  return root;
+}
+ int searchNode(Node *root, T value) {
+  if (root == nullptr)
+   return 0;
+  else if (root->value > value)
+   return searchNode(root->left, value);
+  else if (root->value < value)
+   return search(root->right, value);
+  else
+   return root->count;
+ }
+ int getheight(Node *root) {
+  if (root == nullptr)
+   return 0;
+  if (root->left == nullptr && root->right == nullptr)
+   return 0;
+  int lh = depthTree(root->left);
+  int rh = depthTree(root->right);
+  return (lh > rh ? lh : rh) + 1;
+ }
  
  public:
-  BST() : root(nullptr) {}
-  ~BST() {}
-  void add(T value) {
-    root = addNode(root, value);
-  }
-  int depth() {
-    return getheight(root);
-  }
-  int search(T value) {
-    return searchNode(root, value);
-  }
+ BST() : root(nullptr) {}
+ ~BST() {}
+ void add(T value) {
+  root = addNode(root, value);
+ }
+ int depth() {
+  return getheight(root);
+ }
+ int search(T value) {
+  return searchNode(root, value);
+ }
 };
 #endif  // INCLUDE_BST_H_
